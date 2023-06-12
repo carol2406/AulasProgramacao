7bb4c32174843f7bbf115af15c633ebd0dc5fad6
Carol <129768904+carol2406@users.noreply.github.com>
2023-06-12 11:30:30 -0300

    Add files via upload

---
+++ Editora.java
@@ -0,0 +1,29 @@
+package Model;
+
+public class Editora {
+	
+	private long idEditora;
+	private String nome;
+	private String emai;
+	
+	
+	public long getIdEditora() {
+		return idEditora;
+	}
+	public void setIdEditora(long idEditora) {
+		this.idEditora = idEditora;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmai() {
+		return emai;
+	}
+	public void setEmai(String emai) {
+		this.emai = emai;
+	}
+
+}
